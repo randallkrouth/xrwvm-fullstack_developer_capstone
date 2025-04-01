627fde52b9b4ebce5744060ae41f7c4459217179
randallkrouth <randy.krouth@gmail.com>
2025-04-01 03:00:34 -0400

    Update

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -110,7 +110,10 @@ def get_dealer_reviews(request, dealer_id):
         for review_detail in reviews:
             response = analyze_review_sentiments(review_detail['review'])
             print(response)
-            review_detail['sentiment'] = response['sentiment']
+            try:
+                review_detail['sentiment'] = response['sentiment'] 
+            except:
+                print("Error")
         return JsonResponse({"status":200,"reviews":reviews})
     else:
         return JsonResponse({"status":400,"message":"Bad Request"})
